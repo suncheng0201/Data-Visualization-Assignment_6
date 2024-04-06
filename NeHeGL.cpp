3aed863150738b82d024e149ed2d5155c23360fe
suncheng0201 <166220337+suncheng0201@users.noreply.github.com>
2024-04-06 21:04:54 +0900

    Create NeHeGL.cpp

---
+++ NeHeGL.cpp
@@ -0,0 +1,442 @@
+#include <windows.h>													// Header File For The Windows Library
+#include <gl/gl.h>														// Header File For The OpenGL32 Library
+#include <gl/glu.h>														// Header File For The GLu32 Library
+#include <new.h>
+#include "NeHeGL.h"													
+
+#define WM_TOGGLEFULLSCREEN (WM_USER+1)								
+
+static BOOL g_isProgramLooping;																												// Between Fullscreen / Windowed Mode
+static BOOL g_createFullScreen;										
+void TerminateApplication (GL_Window* window)						
+{
+	PostMessage (window->hWnd, WM_QUIT, 0, 0);						
+	g_isProgramLooping = FALSE;										
+}
+
+void ToggleFullscreen (GL_Window* window)							
+{
+	PostMessage (window->hWnd, WM_TOGGLEFULLSCREEN, 0, 0);			
+}
+
+void ReshapeGL (int width, int height)								
+{
+	glViewport (0, 0, (GLsizei)(width), (GLsizei)(height));			
+}
+
+BOOL ChangeScreenResolution (int width, int height, int bitsPerPixel)
+{
+	DEVMODE dmScreenSettings;									
+	ZeroMemory (&dmScreenSettings, sizeof (DEVMODE));				
+	dmScreenSettings.dmSize				= sizeof (DEVMODE);			
+	dmScreenSettings.dmPelsWidth		= width;				
+	dmScreenSettings.dmPelsHeight		= height;					
+	dmScreenSettings.dmBitsPerPel		= bitsPerPixel;				
+	dmScreenSettings.dmFields			= DM_BITSPERPEL | DM_PELSWIDTH | DM_PELSHEIGHT;
+	if (ChangeDisplaySettings (&dmScreenSettings, CDS_FULLSCREEN) != DISP_CHANGE_SUCCESSFUL)
+	{
+		return FALSE;												
+	}
+	return TRUE;													
+}
+
+BOOL CreateWindowGL (GL_Window* window)								
+{
+	__int64	timer;													
+	DWORD windowStyle = WS_OVERLAPPEDWINDOW;						
+	DWORD windowExtendedStyle = WS_EX_APPWINDOW;					
+
+	PIXELFORMATDESCRIPTOR pfd =										
+	{
+		sizeof (PIXELFORMATDESCRIPTOR),								
+		1,															
+		PFD_DRAW_TO_WINDOW |										
+		PFD_SUPPORT_OPENGL |										
+		PFD_DOUBLEBUFFER,											
+		PFD_TYPE_RGBA,												
+		window->init.bitsPerPixel,									
+		0, 0, 0, 0, 0, 0,											
+		0,															
+		0,															
+		0,															
+		0, 0, 0, 0,													
+		16,															  
+		0,															
+		0,															
+		PFD_MAIN_PLANE,												
+		0,															
+		0, 0, 0														
+	};
+
+	RECT windowRect = {0, 0, window->init.width, window->init.height};
+
+	GLuint PixelFormat;												
+
+	if (window->init.isFullScreen == TRUE)						
+	{
+		if (ChangeScreenResolution (window->init.width, window->init.height, window->init.bitsPerPixel) == FALSE)
+		{
+			// Fullscreen Mode Failed.  Run In Windowed Mode Instead
+			MessageBox (HWND_DESKTOP, "Mode Switch Failed.\nRunning In Windowed Mode.", "Error", MB_OK | MB_ICONEXCLAMATION);
+			window->init.isFullScreen = FALSE;						
+		}
+		else														
+		{
+			ShowCursor (FALSE);										
+			windowStyle = WS_POPUP;									
+			windowExtendedStyle |= WS_EX_TOPMOST;						
+		}															
+	}
+	else															
+	{
+		// Adjust Window, Account For Window Borders
+		AdjustWindowRectEx (&windowRect, windowStyle, 0, windowExtendedStyle);
+	}
+
+	// Create The OpenGL Window
+	window->hWnd = CreateWindowEx (windowExtendedStyle,					// Extended Style
+								   window->init.application->className,	// Class Name
+								   window->init.title,					// Window Title
+								   windowStyle,							// Window Style
+								   0, 0,								// Window X,Y Position
+								   windowRect.right - windowRect.left,	// Window Width
+								   windowRect.bottom - windowRect.top,	// Window Height
+								   HWND_DESKTOP,						// Desktop Is Window's Parent
+								   0,									// No Menu
+								   window->init.application->hInstance, // Pass The Window Instance
+								   window);
+
+	if (window->hWnd == 0)												// Was Window Creation A Success?
+	{
+		return FALSE;													// If Not Return False
+	}
+
+	window->hDC = GetDC (window->hWnd);									// Grab A Device Context For This Window
+	if (window->hDC == 0)												// Did We Get A Device Context?
+	{
+		// Failed
+		DestroyWindow (window->hWnd);									// Destroy The Window
+		window->hWnd = 0;												// Zero The Window Handle
+		return FALSE;													// Return False
+	}
+
+	PixelFormat = ChoosePixelFormat (window->hDC, &pfd);				// Find A Compatible Pixel Format
+	if (PixelFormat == 0)												// Did We Find A Compatible Format?
+	{
+		// Failed
+		ReleaseDC (window->hWnd, window->hDC);							// Release Our Device Context
+		window->hDC = 0;												// Zero The Device Context
+		DestroyWindow (window->hWnd);									// Destroy The Window
+		window->hWnd = 0;												// Zero The Window Handle
+		return FALSE;													// Return False
+	}
+
+	if (SetPixelFormat (window->hDC, PixelFormat, &pfd) == FALSE)		// Try To Set The Pixel Format
+	{
+		// Failed
+		ReleaseDC (window->hWnd, window->hDC);							// Release Our Device Context
+		window->hDC = 0;												// Zero The Device Context
+		DestroyWindow (window->hWnd);									// Destroy The Window
+		window->hWnd = 0;												// Zero The Window Handle
+		return FALSE;													// Return False
+	}
+
+	window->hRC = wglCreateContext (window->hDC);						// Try To Get A Rendering Context
+	if (window->hRC == 0)												// Did We Get A Rendering Context?
+	{
+		// Failed
+		ReleaseDC (window->hWnd, window->hDC);							// Release Our Device Context
+		window->hDC = 0;												// Zero The Device Context
+		DestroyWindow (window->hWnd);									// Destroy The Window
+		window->hWnd = 0;												// Zero The Window Handle
+		return FALSE;													// Return False
+	}
+
+	// Make The Rendering Context Our Current Rendering Context
+	if (wglMakeCurrent (window->hDC, window->hRC) == FALSE)
+	{
+		// Failed
+		wglDeleteContext (window->hRC);									// Delete The Rendering Context
+		window->hRC = 0;												// Zero The Rendering Context
+		ReleaseDC (window->hWnd, window->hDC);							// Release Our Device Context
+		window->hDC = 0;												// Zero The Device Context
+		DestroyWindow (window->hWnd);									// Destroy The Window
+		window->hWnd = 0;												// Zero The Window Handle
+		return FALSE;													// Return False
+	}
+
+	ShowWindow (window->hWnd, SW_NORMAL);								// Make The Window Visible
+	window->isVisible = TRUE;											// Set isVisible To True
+
+	ReshapeGL (window->init.width, window->init.height);				// Reshape Our GL Window
+
+	ZeroMemory (window->keys, sizeof (Keys));							// Clear All Keys
+
+	if (QueryPerformanceFrequency((LARGE_INTEGER *) &timer))			// Check To See If We Can Use Peformance Counter
+	{
+		window->hrTimer=TRUE;											// High Resolution Is Available
+		// Grab The Starting Tick Value
+		window->lastTickCount = QueryPerformanceCounter((LARGE_INTEGER *) &timer);
+		// Grab The Counter Frequency
+		QueryPerformanceFrequency((LARGE_INTEGER *) &timer);
+		// Set The Timer Resolution 1.0f / Timer Frequency
+		window->timerResolution = (float) (((double)1.0f)/((double)timer));
+	}
+	else
+	{
+		window->lastTickCount = GetTickCount ();						// Get Tick Count The Old Fashioned Way
+		window->timerResolution = 1.0f/1000.0f;							// Set Our Timer Resolution To .001f
+	}
+
+	return TRUE;														// Window Creating Was A Success
+																		// Initialization Will Be Done In WM_CREATE
+}
+
+BOOL DestroyWindowGL (GL_Window* window)								// Destroy The OpenGL Window & Release Resources
+{
+	if (window->hWnd != 0)												// Does The Window Have A Handle?
+	{	
+		if (window->hDC != 0)											// Does The Window Have A Device Context?
+		{
+			wglMakeCurrent (window->hDC, 0);							// Set The Current Active Rendering Context To Zero
+			if (window->hRC != 0)										// Does The Window Have A Rendering Context?
+			{
+				wglDeleteContext (window->hRC);							// Release The Rendering Context
+				window->hRC = 0;										// Zero The Rendering Context
+			}
+			ReleaseDC (window->hWnd, window->hDC);						// Release The Device Context
+			window->hDC = 0;											// Zero The Device Context
+		}
+		DestroyWindow (window->hWnd);									// Destroy The Window
+		window->hWnd = 0;												// Zero The Window Handle
+	}
+
+	if (window->init.isFullScreen)										// Is Window In Fullscreen Mode
+	{
+		ChangeDisplaySettings (NULL,0);									// Switch Back To Desktop Resolution
+		ShowCursor (TRUE);												// Show The Cursor
+	}	
+	return TRUE;														// Return True
+}
+
+// Process Window Message Callbacks
+LRESULT CALLBACK WindowProc (HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	DWORD		tickCount;												// Holds Current Tick Count
+	__int64		timer;													// Used For The Tick Counter
+
+	// Get The Window Context
+	GL_Window* window = (GL_Window*)(GetWindowLong (hWnd, GWL_USERDATA));
+
+	switch (uMsg)														// Evaluate Window Message
+	{
+		case WM_ERASEBKGND:												// Check To See If Windows Is Trying To Erase The Background
+			return 0;													// Return 0 (Prevents Flickering While Resizing A Window)
+
+		case WM_PAINT:													// Window Needs Updating
+		{
+			if (window->hrTimer)										// Are We Using High Resolution Timer
+			{
+				QueryPerformanceCounter((LARGE_INTEGER *) &timer);		// Grab Current Value In Performance Counter
+				tickCount = (DWORD) timer;								// Store It In Tick Count
+			}
+			else														// Otherwise We Grab Ticks The Old Fashioned Way
+				tickCount = GetTickCount ();							// Get The Tick Count
+
+			// Call Update With The New Counter Value
+			Update ((tickCount - window->lastTickCount) * window->timerResolution * 1000.0f);
+			window->lastTickCount = tickCount;							// Set Last Count To Current Count
+			Draw ();													// Draw Our Scene
+			SwapBuffers (window->hDC);									// Swap Buffers (Double Buffering)
+		}
+		return 0;														// Return
+
+		case WM_SYSCOMMAND:												// Intercept System Commands
+		{
+			switch (wParam)												// Check System Calls
+			{
+				case SC_SCREENSAVE:										// Screensaver Trying To Start?
+				case SC_MONITORPOWER:									// Monitor Trying To Enter Powersave?
+				return 0;												// Prevent From Happening
+			}
+			break;														// Exit
+		}
+		return 0;														// Return
+
+		case WM_CREATE:													// Window Creation
+		{
+			CREATESTRUCT* creation = (CREATESTRUCT*)(lParam);			// Store Window Structure Pointer
+			window = (GL_Window*)(creation->lpCreateParams);
+			SetWindowLong (hWnd, GWL_USERDATA, (LONG)(window));
+		}
+		return 0;														// Return
+
+		case WM_CLOSE:													// Closing The Window
+			TerminateApplication(window);								// Terminate The Application
+		return 0;														// Return
+
+		case WM_SIZE:													// Size Action Has Taken Place
+			switch (wParam)												// Evaluate Size Action
+			{
+				case SIZE_MINIMIZED:									// Was Window Minimized?
+					window->isVisible = FALSE;							// Set isVisible To False
+				return 0;												// Return
+
+				case SIZE_MAXIMIZED:									// Was Window Maximized?
+					window->isVisible = TRUE;							// Set isVisible To True
+					ReshapeGL (LOWORD (lParam), HIWORD (lParam));		// Reshape Window - LoWord=Width, HiWord=Height
+				return 0;												// Return
+
+				case SIZE_RESTORED:										// Was Window Restored?
+					window->isVisible = TRUE;							// Set isVisible To True
+					ReshapeGL (LOWORD (lParam), HIWORD (lParam));		// Reshape Window - LoWord=Width, HiWord=Height
+				return 0;												// Return
+			}
+		break;															// Break
+
+		case WM_KEYDOWN:												// Update Keyboard Buffers For Keys Pressed
+			if ((wParam >= 0) && (wParam <= 255))						// Is Key (wParam) In A Valid Range?
+			{
+				window->keys->keyDown [wParam] = TRUE;					// Set The Selected Key (wParam) To True
+				return 0;												// Return
+			}
+		break;															// Break
+
+		case WM_KEYUP:													// Update Keyboard Buffers For Keys Released
+			if ((wParam >= 0) && (wParam <= 255))						// Is Key (wParam) In A Valid Range?
+			{
+				window->keys->keyDown [wParam] = FALSE;					// Set The Selected Key (wParam) To False
+				return 0;												// Return
+			}
+		break;															// Break
+
+		case WM_TOGGLEFULLSCREEN:										// Toggle FullScreen Mode On/Off
+			g_createFullScreen = (g_createFullScreen == TRUE) ? FALSE : TRUE;
+			PostMessage (hWnd, WM_QUIT, 0, 0);
+		break;															// Break
+	}
+
+	return DefWindowProc (hWnd, uMsg, wParam, lParam);					// Pass Unhandled Messages To DefWindowProc
+}
+
+BOOL RegisterWindowClass (Application* application)						// Register A Window Class For This Application.
+{																		// TRUE If Successful
+	// Register A Window Class
+	WNDCLASSEX windowClass;												// Window Class
+	ZeroMemory (&windowClass, sizeof (WNDCLASSEX));						// Make Sure Memory Is Cleared
+	windowClass.cbSize			= sizeof (WNDCLASSEX);					// Size Of The windowClass Structure
+	windowClass.style			= CS_HREDRAW | CS_VREDRAW | CS_OWNDC;	// Redraws The Window For Any Movement / Resizing
+	windowClass.lpfnWndProc		= (WNDPROC)(WindowProc);				// WindowProc Handles Messages
+	windowClass.hInstance		= application->hInstance;				// Set The Instance
+	windowClass.hbrBackground	= (HBRUSH)COLOR_WINDOW+1;				// Class Background Brush Color
+	windowClass.hCursor			= LoadCursor(NULL, IDC_ARROW);			// Load The Arrow Pointer
+	windowClass.lpszClassName	= application->className;				// Sets The Applications Classname
+	if (RegisterClassEx (&windowClass) == 0)							// Did Registering The Class Fail?
+	{
+		// NOTE: Failure, Should Never Happen
+		MessageBox (HWND_DESKTOP, "RegisterClassEx Failed!", "Error", MB_OK | MB_ICONEXCLAMATION);
+		return FALSE;													// Return False (Failure)
+	}
+	return TRUE;														// Return True (Success)
+}
+
+// Program Entry (WinMain)
+int WINAPI WinMain (HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	Application			application;									// Application Structure
+	GL_Window			window;											// Window Structure
+	Keys				keys;											// Key Structure
+	BOOL				isMessagePumpActive;							// Message Pump Active?
+	MSG					msg;											// Window Message Structure
+
+	// Fill Out Application Data
+	application.className = "OpenGL";									// Application Class Name
+	application.hInstance = hInstance;									// Application Instance
+
+	// Fill Out Window
+	ZeroMemory (&window, sizeof (GL_Window));							// Make Sure Memory Is Zeroed
+	window.keys					= &keys;								// Window Key Structure
+	window.init.application		= &application;							// Window Application
+
+	// Window Title
+	window.init.title			= "19170107_孙铖_第6次作业_迷宫创建成功！";
+
+	window.init.width			= 1024;									// Window Width
+	window.init.height			= 768;									// Window Height
+	window.init.bitsPerPixel	= 32;									// Bits Per Pixel
+	window.init.isFullScreen	= TRUE;									// Fullscreen? (Set To TRUE)
+
+	ZeroMemory (&keys, sizeof (Keys));									// Zero keys Structure
+
+	// Ask The User If They Want To Start In FullScreen Mode? (Remove These 4 Lines If You Want To Force Fullscreen)
+	if (MessageBox (HWND_DESKTOP, "Would You Like To Run In Fullscreen Mode?", "Start FullScreen?", MB_YESNO | MB_ICONQUESTION) == IDNO)
+	{
+		window.init.isFullScreen = FALSE;								// If Not, Run In Windowed Mode
+	}
+
+	// Register A Class For Our Window To Use
+	if (RegisterWindowClass (&application) == FALSE)					// Did Registering A Class Fail?
+	{
+		// Failure
+		MessageBox (HWND_DESKTOP, "Error Registering Window Class!", "Error", MB_OK | MB_ICONEXCLAMATION);
+		return -1;														// Terminate Application
+	}
+
+	g_isProgramLooping = TRUE;											// Program Looping Is Set To TRUE
+	g_createFullScreen = window.init.isFullScreen;						// g_createFullScreen Is Set To User Default
+	while (g_isProgramLooping)											// Loop Until WM_QUIT Is Received
+	{
+		// Create A Window
+		window.init.isFullScreen = g_createFullScreen;					// Set Init Param Of Window Creation To Fullscreen?
+		if (CreateWindowGL (&window) == TRUE)							// Was Window Creation Successful?
+		{
+			// At This Point We Should Have A Window That Is Setup To Render OpenGL
+			if (Initialize (&window, &keys) == FALSE)					// Call User Initialization
+			{
+				// Failure
+				TerminateApplication (&window);							// Close Window, This Will Handle The Shutdown
+			}
+			else														// Otherwise (Start The Message Pump)
+			{	// Initialize was a success
+				isMessagePumpActive = TRUE;								// Set isMessagePumpActive To TRUE
+				while (isMessagePumpActive == TRUE)						// While The Message Pump Is Active
+				{
+					// Success Creating Window.  Check For Window Messages
+					if (PeekMessage (&msg, window.hWnd, 0, 0, PM_REMOVE) != 0)
+					{
+						// Check For WM_QUIT Message
+						if (msg.message != WM_QUIT)						// Is The Message A WM_QUIT Message?
+						{
+							DispatchMessage (&msg);						// If Not, Dispatch The Message
+						}
+						else											// Otherwise (If Message Is WM_QUIT)
+						{
+							isMessagePumpActive = FALSE;				// Terminate The Message Pump
+						}
+					}
+					else												// If There Are No Messages
+					{
+						if (window.isVisible == FALSE)					// If Window Is Not Visible
+						{
+							WaitMessage ();								// Application Is Minimized Wait For A Message
+						}
+					}
+				}														// Loop While isMessagePumpActive == TRUE
+			}															// If (Initialize (...
+
+			// Application Is Finished
+			Deinitialize ();											// User Defined DeInitialization
+
+			DestroyWindowGL (&window);									// Destroy The Active Window
+		}
+		else															// If Window Creation Failed
+		{
+			// Error Creating Window
+			MessageBox (HWND_DESKTOP, "Error Creating OpenGL Window", "Error", MB_OK | MB_ICONEXCLAMATION);
+			g_isProgramLooping = FALSE;									// Terminate The Loop
+		}
+	}																	// While (isProgramLooping)
+
+	UnregisterClass (application.className, application.hInstance);		// UnRegister Window Class
+	return 0;
+}																		// End Of WinMain()
